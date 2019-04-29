b587527ed5f2265026af65f1d45736e4270426ea
Shivam Bansal <shivam2297@gmail.com>
2019-04-29 16:27:32 +0530

    Testing code for background execution.

--- GCPFileUpload demo/Screens/SchedulerScreen/view/TaskSchedulerViewController.swift
+++ GCPFileUpload demo/Screens/SchedulerScreen/view/TaskSchedulerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class TaskSchedulerViewController: UIViewController {
 
@@ -20,9 +21,52 @@ class TaskSchedulerViewController: UIViewController {
     }
 
 
+    override func viewDidAppear(_ animated: Bool) {
+//        while 1 < 2 {
+//            DispatchQueue(label: "task").asyncAfter(deadline: DispatchTime(uptimeNanoseconds: UInt64(5000))) {
+//                let r: Int = Int.random(in: ClosedRange(uncheckedBounds: (lower: 0, upper: 200)))
+//                print("performing task: \(r)")
+////                self.showNotifications(r: r)
+//            }
+//        }
+    }
+
+
+    func showNotifications(r: Int) {
+        let content = UNMutableNotificationContent()
+        content.title = "Background Task"
+        content.body = "performing task: \(r)"
+
+        //        content.categoryIdentifier = "RE_NOTIFY"
+
+        //        content.sound = UNNotificationSound.default
+
+        let notifyRequest = UNNotificationRequest(identifier: "task", content: content, trigger: UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(exactly: 0.1)!, repeats: false))
+
+        //        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+
+        UNUserNotificationCenter.current().add(notifyRequest) { (error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
 
 }
 
 extension TaskSchedulerViewController: TaskSchedulerViewDelegate {
+
+}
+
+extension TaskSchedulerViewController: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+    }
 
 }
